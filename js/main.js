6bf62f56576e75fe34f2904548dcd4373010617c
Oleksandr <92778018+SANCHOS4004@users.noreply.github.com>
2024-10-17 20:12:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+function change_page(path_to) { // Zmienia strone
+    window.location.href = "./pages/" + path_to + '.html'
+}
--- js/pages.js
+++ js/pages.js
@@ -1,5 +1,10 @@
+
 const home_btn = document.getElementById('homeButton')
 
-        home_btn.addEventListener('click', function() {
-            window.location.href = '/index.html';
-        });
+home_btn.addEventListener('click', function() { // Zwraca na pierwszą strone
+    window.location.href = '../index.html';
+});
+
+function scroll_to_top() { // Scroll w góre
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
